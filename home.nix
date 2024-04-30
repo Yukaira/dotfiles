ecf8f4f83816be86014c501e146c37eca0f6ae08
yukaira <ben70918@gmail.com>
2024-04-30 19:56:21 -0400

    added handbrake

--- home.nix
+++ home.nix
@@ -10,6 +10,7 @@
 
     packages = with pkgs; [
         # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+        handbrake
         micro
         ani-cli
         discord
