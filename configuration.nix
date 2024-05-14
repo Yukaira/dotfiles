85aa06494bb4528c2c56088ed5523153a5cd5b42
yukaira <ben70918@gmail.com>
2024-05-14 19:41:12 -0400

    Switched to plasma6

--- configuration.nix
+++ configuration.nix
@@ -34,10 +34,11 @@
   i18n.defaultLocale = "en_CA.UTF-8";
 
 
-  # Enable the KDE Plasma Desktop Environment.
+  # Enable Plasma6
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
+  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+   
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
