dbb635b7e0e932bf3c8fe540e9f3575d2fdffa19
yukaira <ben70918@gmail.com>
2025-03-29 23:21:54 -0400

    disable starship

--- home.nix
+++ home.nix
@@ -118,25 +118,25 @@
     };
 
   };
-    # Enable & Configure Starship
-    programs.starship =
-     let
-       flavour = "mocha"; # One of `latte`, `frappe`, `macchiato`, or `mocha`
-     in
-     {
-       enable = true;
-       settings = {
-         # Other config here
-         format = "$all"; # Remove this line to disable the default prompt format
-         palette = "catppuccin_${flavour}";
-       } // builtins.fromTOML (builtins.readFile
-         (pkgs.fetchFromGitHub
-           {
-             owner = "catppuccin";
-             repo = "starship";
-             rev = "5629d23"; # Replace with the latest commit hash
-             sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
-           } + /palettes/${flavour}.toml));
-     };
+#    # Enable & Configure Starship
+#    programs.starship =
+#     let
+#       flavour = "mocha"; # One of `latte`, `frappe`, `macchiato`, or `mocha`
+#     in
+#     {
+#       enable = true;
+#       settings = {
+#         # Other config here
+#        format = "$all"; # Remove this line to disable the default prompt format
+#         palette = "catppuccin_${flavour}";
+#       } // builtins.fromTOML (builtins.readFile
+#         (pkgs.fetchFromGitHub
+#           {
+#             owner = "catppuccin";
+#             repo = "starship";
+#            rev = "c41c89f"; # Replace with the latest commit hash
+#             sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
+#           } + /palettes/${flavour}.toml));
+#     };
 
 }
