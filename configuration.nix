6f4b349a18cf0700ee597606b3f676c5ffb9c9ad
yukaira <ben70918@gmail.com>
2025-10-13 23:02:32 -0400

    use Lix instead of Nix :3

--- configuration.nix
+++ configuration.nix
@@ -32,6 +32,17 @@
     }
   ];
 
+  # lix
+    nixpkgs.overlays = [ (final: prev: {
+      inherit (prev.lixPackageSets.stable)
+        nixpkgs-review
+        nix-eval-jobs
+        nix-fast-build
+        colmena;
+    }) ];
+  
+    nix.package = pkgs.lixPackageSets.stable.lix;
+  
 
   # Define hostname
   networking.hostName = "protogen-13"; 
