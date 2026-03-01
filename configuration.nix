a1aadd469a9dc496b18bb7b7a869e1fb03c0c807
yukaira <ben70918@gmail.com>
2026-02-28 19:31:59 -0500

    remove cap_sys_nice patch

--- configuration.nix
+++ configuration.nix
@@ -21,16 +21,18 @@
     device = "nodev";
     efiSupport = true;
   };
-  boot.kernelPatches = [
-    {
-      name = "amdgpu-ignore-ctx-privileges";
-      patch = pkgs.fetchpatch {
-        name = "cap_sys_nice_begone.patch";
-        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-      };
-    }
-  ];
+
+# patch for VR
+# boot.kernelPatches = [
+#    {
+#      name = "amdgpu-ignore-ctx-privileges";
+#      patch = pkgs.fetchpatch {
+#        name = "cap_sys_nice_begone.patch";
+#        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+#        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+#      };
+#    }
+#  ];
 
   # lix
   nixpkgs.overlays = [
